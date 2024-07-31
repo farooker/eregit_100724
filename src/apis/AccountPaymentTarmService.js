47349c25f83dae5603bccaee1ec8e3fd5d7aaae3
alif <delatforce19436@gmail.com>
2024-07-31 17:55:41 +0700

    sort account payment terms

--- src/apis/AccountPaymentTarmService.js
+++ src/apis/AccountPaymentTarmService.js
@@ -1,13 +1,12 @@
 import axiosBase from "./AxiosConfig";
-const getAccounrAccountPaymentTarmsAll= async () => {
+const getAccounrAccountPaymentTarmsAll = async () => {
   return await axiosBase({
     method: "get",
-    url: `/master/get-account-payment-terms`,
+    url: `/master/get-account-payment-terms?sort-by=id:desc`,
     data: {},
   });
 };
 
 export default {
-    getAccounrAccountPaymentTarmsAll,
-  };
-  
+  getAccounrAccountPaymentTarmsAll,
+};
